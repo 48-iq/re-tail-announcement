4d512a13e67b6f0066779bc400413a9229fa39b7
48-iq <ivn25ov@mail.ru>
2025-03-22 14:07:00 +0400

    init

---
+++ src/test/java/dev/ilya_anna/announcement_service/AnnouncementServiceApplicationTests.java
@@ -0,0 +1,8 @@
+package dev.ilya_anna.announcement_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnouncementServiceApplicationTests {
+}
